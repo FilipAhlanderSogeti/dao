2465711d383917a77cd32c78595dcfb8a6efe42e
Filip Åhlander <filip.a.ahlander@sogeti.se>
2022-05-18 18:07:13 +0200

    ta pengar

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -2,26 +2,30 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol"; 
+
+// import "@openzeppelin/contracts/access/Ownable.sol";
 // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
 contract YourContract {
+    event SetPurpose(address sender, uint256 purpose);
 
-  event SetPurpose(address sender, string purpose);
+    // string public purpose = "Building Unstoppable Apps!!!";
+    address pengapung = 0xccFDBBDD2512F48fE394F16431f802Df84299378;
 
-  string public purpose = "Building Unstoppable Apps!!!";
+    constructor() payable {
+        // what should we do on deploy?
+    }
 
-  constructor() payable {
-    // what should we do on deploy?
-  }
+    function setPurpose(uint256 newPurpose) public payable {
+        payable(pengapung).transfer((msg.value * 1) / 10);
+        if (newPurpose == 10) {
+            console.log("We have a wiener");
+            payable(msg.sender).transfer(address(this).balance);
+        }
+    }
 
-  function setPurpose(string memory newPurpose) public {
-      purpose = newPurpose;
-      console.log(msg.sender,"set purpose to",purpose);
-      emit SetPurpose(msg.sender, purpose);
-  }
+    // to support receiving ETH by default
+    receive() external payable {}
 
-  // to support receiving ETH by default
-  receive() external payable {}
-  fallback() external payable {}
+    fallback() external payable {}
 }
